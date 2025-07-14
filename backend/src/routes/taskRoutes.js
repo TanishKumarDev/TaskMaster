9a2f7edda88404ef3fad5728e37e13e44d3e34d8
Tanish <tanish.kumar.contact@gmail.com>
2025-07-14 13:09:30 +0530

    Add getSubTask route to taskRoutes for retrieving specific sub-tasks

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { createTask, getTasks, getTaskById, updateTask, deleteTask, addSubTask, updateSubTask, deleteSubTask, searchTasks } from '../controllers/taskController.js';
+import { createTask, getTasks, getTaskById, updateTask, deleteTask, addSubTask, updateSubTask, deleteSubTask, searchTasks, getSubTask } from '../controllers/taskController.js';
 
 const router = express.Router();
 
+// Search tasks
 router.get('/search', protect, searchTasks);
+
+// Task routes
 router.post('/', protect, createTask);
 router.get('/', protect, getTasks);
 router.get('/:id', protect, getTaskById);
 router.put('/:id', protect, updateTask);
 router.delete('/:id', protect, deleteTask);
+
+// Sub-task routes
+router.get('/:taskId/subtasks/:subTaskId', protect, getSubTask);
 router.post('/:taskId/subtasks', protect, addSubTask);
 router.put('/:taskId/subtasks/:subTaskId', protect, updateSubTask);
 router.delete('/:taskId/subtasks/:subTaskId', protect, deleteSubTask);
